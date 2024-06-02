0ab9c25f66377121d3459744897b41b9f6bdc845
Monday_David_Sarka <davidsarka242@gmail.com>
2024-06-02 15:04:51 +0100

    Created a state to hold new columns

--- src/components/KanbanBoad.tsx
+++ src/components/KanbanBoad.tsx
@@ -1,16 +1,34 @@
+import { useState } from "react";
 import PlusIcon from "../icons/PlusIcon";
+import { Column } from "../types";
 
 function KanbanBoad() {
-  
+  const [columns, setColumns] = useState<Column[]>([]);
+
   return (
     <div className="mx-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
       <div className="mx-auto">
-      <button className="flex gap-2 p-4 ring-rose-500 hover:ring-2 h-[50px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor">
-        <PlusIcon />
-        Add Column
-      </button>
+        <button
+          onClick={createNewColumn}
+          className="flex gap-2 p-4 ring-rose-500 hover:ring-2 h-[50px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor"
+        >
+          <PlusIcon />
+          Add Column
+        </button>
       </div>
     </div>
   );
+
+  const createNewColumn = () => {
+    const columndToAdd: Column = {
+      id: generateId(),
+      title: `Column ${columns.length + 1}`,
+    };
+    setColumns([...columns, columndToAdd]);
+  };
+
+  function generateId() {
+    
+  }
 }
 export default KanbanBoad;
