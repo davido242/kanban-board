bca1499e20ac7b2e6690faffc3ca3c5e73fac5e3
David MD <davidsarka242@gmail.com>
2024-07-09 09:02:34 +0100

    Some cleanup

--- src/components/KanbanBoad.tsx
+++ src/components/KanbanBoad.tsx
@@ -8,7 +8,7 @@ import { SortableContext } from "@dnd-kit/sortable";
 function KanbanBoad() {
   const [columns, setColumns] = useState<Column[]>([]);
   const columnsId = useMemo(() => columns.map((col) => col.id), [columns]);
- 
+
   const createNewColumn = () => {
     const columndToAdd: Column = {
       id: generateId(),
@@ -16,32 +16,34 @@ function KanbanBoad() {
     };
     setColumns([...columns, columndToAdd]);
   };
- 
+
   function generateId() {
     return Math.floor(Math.random() * 10001);
   }
-  
+
   const deleteColumn = (id: Id) => {
-    const filteredColumn = columns.filter(column => column.id !== id);
+    const filteredColumn = columns.filter((column) => column.id !== id);
     setColumns(filteredColumn);
-  }
+  };
 
-  const ondragstart = (event: DragStartEvent) => {
-    console.log("Drag start: ", event)
-
+  const onDragStart = (event: DragStartEvent) => {
+    console.log("Drag start: ", event);
   };
   return (
     <div className="mx-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
-      <DndContext onDragStart={ondragstart}>
+      <DndContext onDragStart={onDragStart}>
         <div className="flex mx-auto gap-4">
-          {/* show our new columns */}
           <div className="flex gap-4">
             <SortableContext items={columnsId}>
-            {columns.map(col => 
-            <ColumnContainer key={col.id} column={col} deleteColumn={deleteColumn} />
-            )}
+              {columns.map((col) => (
+                <ColumnContainer
+                  key={col.id}
+                  column={col}
+                  deleteColumn={deleteColumn}
+                />
+              ))}
             </SortableContext>
-          </div>        
+          </div>
           <button
             onClick={createNewColumn}
             className="flex gap-2 p-4 ring-borderRose hover:ring-2 h-[50px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor"
@@ -52,6 +54,6 @@ function KanbanBoad() {
         </div>
       </DndContext>
     </div>
-  ); 
+  );
 }
 export default KanbanBoad;
